e03e4f85f038ccf57429e198dd694f05f101b54f
Emilia Campagnon <emiliacampagnon@gmail.com>
2024-06-19 17:28:28 -0300

    19junio

--- src/components/pages/flota/Flota.jsx
+++ src/components/pages/flota/Flota.jsx
@@ -29,54 +29,37 @@ const Flota = () => {
         </div>
         <div className="contenedorCardVehiculos">
           <div className="cardVehiculos">
-            <p className="titulosVehiculos">Vehículos de gran porte</p>
-            <div className="contenedorVehiculos">
-              <img
-                className="imgVehiculos"
-                src="https://firebasestorage.googleapis.com/v0/b/logincor-f1fb5.appspot.com/o/Grande%20ford%201722%20chasis.jpg?alt=media&token=81be69cf-717c-4003-a901-1a4364b9a2a9"
-                alt=""
-              />
-              <img
-                className="imgVehiculos"
-                src="https://firebasestorage.googleapis.com/v0/b/logincor-f1fb5.appspot.com/o/Grande%20vw%2017280%20semiremolque.png?alt=media&token=35d89097-cd6c-4b90-a6cb-697a18f549d3"
-                alt=""
-              />
+            <div className="contenedorTituloFlota">
+              <p className="titulosVehiculos">Vehículos de gran porte</p>
+            </div>
+
+            <div className="contenedorTextosFlota">
+              <p className="textoFlota">• Camiones semi-remolque</p>
+              <p className="textoFlota">• Chasis</p>
+              <p className="textoFlota">• Chasis acoplado</p>
+              <p className="textoFlota">• Chasis sider</p>
             </div>
           </div>
           <div className="cardVehiculos">
-            <p className="titulosVehiculos">Vehículos de mediano porte</p>
-            <div className="contenedorVehiculos">
-              <img
-                className="imgVehiculos"
-                src="https://firebasestorage.googleapis.com/v0/b/logincor-f1fb5.appspot.com/o/Mediano%20accelo.jpg?alt=media&token=60b8ea11-bb85-42e5-8a03-57cb4128630c"
-                alt=""
-              />
-              <img
-                className="imgVehiculos"
-                src="https://firebasestorage.googleapis.com/v0/b/logincor-f1fb5.appspot.com/o/Mediano%20Iveco%20Daily.jpg?alt=media&token=90ea4ebb-0684-42be-85ed-ae296b4a8971"
-                alt=""
-              />
-              <img
-                className="imgVehiculos"
-                src="https://firebasestorage.googleapis.com/v0/b/logincor-f1fb5.appspot.com/o/Mediano%20transit.jpg?alt=media&token=acb18e5b-6c2b-4a4c-af21-9c70689f8a1d"
-                alt=""
-              />
+            <div className="contenedorTituloFlota">
+              <p className="titulosVehiculos">Vehículos de mediano porte</p>
+            </div>
+            <div className="contenedorTextosFlota">
+              <p className="textoFlota">• Balancín sider</p>
+              <p className="textoFlota">• Utilitatio sider</p>
+              <p className="textoFlota">• Utilitario furgón</p>
+              <p className="textoFlota">• Utilitario con carrocería</p>
             </div>
           </div>
-        </div>
-        <div className="contenedorAux">
-          <p className="titulosVehiculos">Auxiliares</p>
-          <div className="contenedorImgAux">
-            <img
-              className="imgVehiculos"
-              src="https://firebasestorage.googleapis.com/v0/b/logincor-f1fb5.appspot.com/o/autoelevador.jpg?alt=media&token=c2255399-45a8-4c8a-adb9-2a0785b0f4b7"
-              alt=""
-            />
-            <img
-              className="imgVehiculos"
-              src="https://firebasestorage.googleapis.com/v0/b/logincor-f1fb5.appspot.com/o/apiladora%20toyota.jpg?alt=media&token=0124a15f-166b-4e53-9a53-b17f19e08714"
-              alt=""
-            />
+          <div className="cardVehiculos">
+            <div className="contenedorTituloFlota">
+              <p className="titulosVehiculos">Auxiliares</p>
+            </div>
+            <div className="contenedorTextosFlota">
+              <p className="textoFlota">• Autoelevadores 2,5 tn</p>
+              <p className="textoFlota">• Autoelevadores 3,5 tn</p>
+              <p className="textoFlota">• Apiladores 8m</p>
+            </div>
           </div>
         </div>
       </div>
