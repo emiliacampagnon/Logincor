6738e6c13718b191a5260b996fe7bad56ed2109f
Emilia Campagnon <emiliacampagnon@gmail.com>
2024-08-22 16:32:46 -0300

    ñdsss

--- src/components/common/mapasContacto/MapaSantaFeContacto.jsx
+++ src/components/common/mapasContacto/MapaSantaFeContacto.jsx
@@ -1,18 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import "./MapaSantaFeContacto.css";
 
 const MapaSantaFeContacto = () => {
-  const position = [-31.6585842, -60.7289004];
+  const position = [-31.6585842, -60.7289004]; // Latitud y longitud de la ubicación central
+
+  const [iconSize, setIconSize] = useState([30, 30]); // Tamaño inicial del ícono
+
+  useEffect(() => {
+    // Función para ajustar el tamaño del ícono según el tamaño de la pantalla
+    const handleResize = () => {
+      if (window.innerWidth <= 480) {
+        setIconSize([20, 20]); // Tamaño reducido para pantallas pequeñas
+      } else {
+        setIconSize([30, 30]); // Tamaño normal para pantallas más grandes
+      }
+    };
+
+    // Agregar el event listener para detectar cambios de tamaño de pantalla
+    window.addEventListener("resize", handleResize);
+
+    // Llamar a la función una vez al cargar para establecer el tamaño correcto inicialmente
+    handleResize();
+
+    // Limpiar el event listener al desmontar el componente
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   const customIcon = new L.Icon({
     iconUrl:
       "https://firebasestorage.googleapis.com/v0/b/logincor-f1fb5.appspot.com/o/ubicacion.png?alt=media&token=c562789b-d1df-4fed-af18-2f80226aa40b", // URL del ícono
-    iconSize: [30, 30], // Tamaño del ícono [ancho, alto]
-    iconAnchor: [16, 32], // Punto en el que se ancla el ícono (debe ser la base del ícono)
-    popupAnchor: [0, -32], // Ajusta el punto de anclaje del popup
+    iconSize: iconSize, // Tamaño del ícono dinámico
+    iconAnchor: [iconSize[0] / 2, iconSize[1]], // Ajusta el punto de anclaje según el tamaño
+    popupAnchor: [0, -iconSize[1]], // Ajusta el punto de anclaje del popup
   });
+
   return (
     <MapContainer
       center={position}
